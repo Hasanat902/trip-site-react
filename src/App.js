017cc671db7880e3f092f1dbcef55d24baf87911
Hasanat902 <abulhasanat902@gmail.com>
2023-02-08 15:24:48 +0600

    added booking section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Activities from './components/Activities';
 import Banner from './components/Banner';
+import Booking from './components/Booking';
 import Navbar from './components/Navbar';
 import TopBar from './components/TopBar';
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar></Navbar>
       <Banner></Banner>
       <Activities></Activities>
+      <Booking></Booking>
     </div>
   );
 }
