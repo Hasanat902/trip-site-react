f2e247c873fdfddba0f5d4ed3f42f746cefb0719
Hasanat902 <abulhasanat902@gmail.com>
2023-02-08 16:04:48 +0600

    added contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Activities from './components/Activities';
 import Banner from './components/Banner';
 import Booking from './components/Booking';
+import Contact from './components/Contact';
 import Gallery from './components/Gallery';
 import Navbar from './components/Navbar';
 import TopBar from './components/TopBar';
@@ -15,6 +16,7 @@ function App() {
       <Activities></Activities>
       <Booking></Booking>
       <Gallery></Gallery>
+      <Contact></Contact>
     </div>
   );
 }
