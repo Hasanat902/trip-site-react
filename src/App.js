39e5981fdc36ad9dd3f652754959b725c4a36a20
Hasanat902 <abulhasanat902@gmail.com>
2023-02-08 15:42:14 +0600

    added gallery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Activities from './components/Activities';
 import Banner from './components/Banner';
 import Booking from './components/Booking';
+import Gallery from './components/Gallery';
 import Navbar from './components/Navbar';
 import TopBar from './components/TopBar';
 
@@ -13,6 +14,7 @@ function App() {
       <Banner></Banner>
       <Activities></Activities>
       <Booking></Booking>
+      <Gallery></Gallery>
     </div>
   );
 }
